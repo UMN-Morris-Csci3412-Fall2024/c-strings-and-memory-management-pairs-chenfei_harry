5baba97e3374d7fe2c17d4a25ada8c42ee647ba6
Harry <lv000013@morris.umn.edu>
2024-09-26 20:58:23 -0500

    Write code for disemvowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,34 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+    if (str == NULL) return NULL; 
+
+    int len = strlen(str);
+    int count = 0;
+
+    for (int i = 0; i < len; ++i) {
+        char c = str[i];
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
+            c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+            count++;
+        }
+    }
+
+    char *result = (char *)malloc(count + 1); 
+    if (result == NULL) return NULL;
+
+
+    int j = 0; 
+    for (int i = 0; i < len; ++i) {
+        char c = str[i];
+        if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
+            c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+            result[j++] = c; 
+        }
+    }
+
+    result[j] = '\0'; 
+    return result;
 }
